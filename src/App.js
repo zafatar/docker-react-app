aefdbd0bd18ee17410fe1a47b21b2cf274fe7d69
zafatar <zaferkiziltoprak@gmail.com>
2021-07-01 14:40:55 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I've got a fast car. I wanna ticket to anywhere.
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
